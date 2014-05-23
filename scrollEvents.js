0dd90fb86caa9e8ae2cd1e1434b0559c23b0f304
Charles Thibault <cthibault@o2web.ca>
2014-05-23 15:34:46 -0400

    methods, the right way

--- scrollEvents.js
+++ scrollEvents.js
@@ -24,39 +24,44 @@ window.se = {
 	$.extend($.fn, {
 		scrollEvents :function(args){
 			if(typeof(args)=='string'){
+
 				if(args=='destroy'){
 					$(window).off('scroll', eventScroller).off('resize',resizeScroller);
 					window.events = [];
 				}
-				else if(args=='trigger'){
-					eventScroller('update');
-				}
 				else if(args=='resize'){
 					resizeScroller();
 				}
+				else if(args=='trigger'){
+					eventScroller('update');
+				}
 				else if(args=='update'){
-					resizeScroller(eventScroller('update'));
+					resizeScroller(eventScroller);
 				}
 				else if(args=='disable' || args=='enable' || args=='remove'){
-					var elements = $(this);
+					var selection = $(this);
 					var removed = [];
-					for(var i=0; i<window.se.events.length; i++){
-						var e = window.se.events[i];
-						for(var j=0; j<elements.length; j++){
-							var element = elements[j];
-							if(e.selector[0] == element){
-								if(args=='remove'){
-									removed.push(i);
-								}else{
-									e.disabled = args=='disable';
-								}
+					for(var i=0; i<selection.length; i++){
+						var element = selection[i];
+						if(element.se){
+							var ev = window.se.events[element.se];
+							if(args=='remove'){
+								removed.push(element.se);
+							}else{
+								ev.disabled = args=='disable';
 							}
 						}
 					}
 					if(args=='remove'){
-						var ev = window.se.events;
+						var es = window.se.events;
+						removed.sort(function(a, b){return b-a});
 						for(var k=0;k<removed.length; k++){
-							ev.splice(removed[k],1);
+							var e = es[removed[k]];
+							if(e && !e.once) $(window).off('scroll', e.visibleFn);
+							es.splice(removed[k],1);
+						}
+						for(var i=0; i<window.se.events.length; i++){
+							window.se.events[i].selector[0].se = i;
 						}
 					}
 				}
@@ -64,7 +69,6 @@ window.se = {
 			}
 			
 			$(this).each(function(k,v){	
-
 				var e = $.extend(true,{
 						selector: $(this),
 						visibleFn: false,
@@ -83,8 +87,14 @@ window.se = {
 				window.se.events.push(e);
 			});
 			
+			for(var i=0; i<window.se.events.length; i++){
+				window.se.events[i].selector[0].se = i;
+			}
+
 			$(window).off('scroll', eventScroller).off('resize',resizeScroller);
 			$(window).on('scroll', eventScroller).on('resize',resizeScroller);
+			
+			return this;
 		}			
 	});
 
@@ -96,22 +106,25 @@ window.se = {
 			var e = se.events[i];
 			if(!e.disabled){
 				if((e.visible||arg=='update') && e.b <= se.t){
-					if(!e.once&&arg!='update'){
+					e.visible=false;
+					if(!e.once){
 						$(window).off('scroll', e.visibleFn);
 					}
 					if(e.upFn && typeof(e.upFn)=='function'){
 						e.upFn();
 					}
-					e.visible=false;
+					
 				}else if((e.visible||arg=='update') && e.t >= se.b){
+					e.visible=false;
 					if(!e.once&&arg!='update'){
 						$(window).off('scroll', e.visibleFn);
 					}
 					if(e.downFn && typeof(e.downFn)=='function'){
 						e.downFn();
 					}
-					e.visible=false;
+					
 				}else if((!e.visible||arg=='update') && e.t < se.b && e.b > se.t && e.visibleFn){
+					e.visible=true;
 					if(!e.once){
 						var j = i+0;
 						$(window).on('scroll', {
@@ -120,37 +133,57 @@ window.se = {
 								index: e.i,
 								height: e.h
 							}, e.visibleFn);
+
+						if(arg=='update') e.visibleFn({
+							data:{
+								delta: function(){return Math.round( ( se.t - (se.events[j].t - se.wh) ) / ( se.events[j].h + se.wh) *100)/100 },
+								selector: e.selector,
+								index: e.i,
+								height: e.h
+							},
+							visible: e.visible
+						});
 					}
-					if(e.once||arg=='update'){
+					if(e.once){
 						e.visibleFn(e);
 					}
-					e.visible=true;
+					
 				}
 			}
 		}
 	}
 
 	var resizeTimeout;
-	
+
+	function recalculate(){
+		var se = window.se;
+		se.wh = $(window).height();
+		for(var i=0; i<se.events.length;i++){
+			var e = se.events[i];
+			var o = e.selector;
+			e.h =  $(o).outerHeight();
+			var tmp = $(o)[0].style.display;
+			$(o)[0].style.display = 'initial';
+			e.t = $(o).offset().top - e.offset;
+			$(o)[0].style.display = tmp;
+			if($(o).attr('style')== '') $(this).removeAttr('style');
+			e.b = e.t+e.h;
+		}
+	}
+
 	function resizeScroller(e){
-		clearTimeout(resizeTimeout);
-		resizeTimeout = setTimeout(function(){
-			var se = window.se;
-			se.wh = $(window).height();
-			for(var i=0; i<se.events.length;i++){
-				var e = se.events[i];
-				var o = e.selector;
-				e.h =  $(o).outerHeight();
-				var tmp = $(o)[0].style.display;
-				$(o)[0].style.display = 'block';
-				e.t = $(o).offset().top - e.offset;
-				$(o)[0].style.display = tmp;
-				if($(o).attr('style')== '') $(this).removeAttr('style');
-				e.t = $(o).offset().top - e.offset;
-				e.b = e.t+e.h;
+			if(typeof(e)=='function'){
+				recalculate();
+				e('update');
 			}
-		},300);
-		if(typeof(e)=='function') e();
+			else{
+				clearTimeout(resizeTimeout);
+				resizeTimeout = setTimeout(function(){
+					recalculate();
+					if(typeof(e)=='object'&&e.type=='resize') eventScroller('update');
+				},150);
+				
+			}
 	}
 
 	resizeScroller();	
