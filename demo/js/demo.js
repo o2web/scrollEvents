0e6ae32275db79f40824c9eed347318a7e42304a
jack <ctibo.ca@gmail.com>
2015-01-19 16:59:53 -0500

    basic parallax, but somethin's wrong....

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,12 +1,30 @@
 jQuery(document).ready(function($){
-	
+
+
+
+  // générer des cercles, parce que les placer à la main c'est trop long
+  var $circles = $('.demo-parallax .circles');
+  var count = 50;
+  var colors = ['turquoise','blue','violet'];
+  var zMax = 5;
+  for(var i=0; i<count; i++){
+    var $circle = $('<div class="circle"></div>');
+    $circle.addClass( colors[Math.floor(Math.random()*(colors.length))] );
+    $circle.attr('z', Math.floor(Math.random()*zMax) );
+    $circle.css({
+      top: Math.round(Math.random()*100)+'%',
+      left: Math.round(Math.random()*100)+'%'
+    });
+    $circles.append($circle);
+  }
 
 	// FIRE CODE TAGS
+  $(window).on('load', function(){
+  	var codes = $('code.eval');
+  	for(var i=0; i<codes.length; i++){
+  		eval($(codes[i]).html());
+  	}
 
-	var codes = $('code.eval');
-	for(var i=0; i<codes.length; i++){
-		eval($(codes[i]).html());
-	}
-
+  });
 
 });
--- parallax.js
+++ parallax.js
@@ -10,9 +10,36 @@
 
 (function($){
 	$win = $(window);
-  
+
+  // global variable for parallax.js
+  window.parallax = {};
+  parallax.items = [];
+  parallax.options = {
+    perspective: 200,
+    transform: Modernizr.prefixed('transform')
+  }
+
+  // jquery function
+  $.fn.parallax = function(args, options){
+    $selection = $(this);
+    $selection.each(function(){
+      var $el = $(this);
+      $el[0].z = parseFloat($el.attr('z'));
+
+    }).scrollEvents({
+      flag: 'parallax',
+      travel: function(e){
+        var $el = e.data.selection;
+        var delta = 1 - e.data.delta();
+        var z = $el[0].z;
+        var travel = (parallax.options.perspective*z)
+        var y = delta*travel - (travel/2);
+
+        $el[0].style[parallax.options.transform] = 'translate3d(0,'+y+'px, 0)';
+      }
+    });
+  }
 
 
 
-	
 })(jQuery);
